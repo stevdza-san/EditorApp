ef03962e2f73e1601368e0c9343943169c562995
Stevdza-San <stefan.jovanavich@gmail.com>
2023-02-12 20:02:38 +0100

    Initial commit

---
+++ site/build.gradle.kts
@@ -0,0 +1,49 @@
+import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+    alias(libs.plugins.kobwebx.markdown)
+}
+
+group = "com.stevdza.san"
+version = "1.0-SNAPSHOT"
+
+kobweb {
+    app {
+        index {
+            description.set("Powered by Kobweb")
+        }
+    }
+}
+
+kotlin {
+    configAsKobwebApplication("san", includeServer = true)
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(libs.kobweb.core)
+                implementation(libs.kobweb.silk.core)
+                implementation(libs.kobweb.silk.icons.fa)
+                implementation(libs.kobwebx.markdown)
+             }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(libs.kobweb.api)
+             }
+        }
+    }
+}
---
+++ site/src/jsMain/kotlin/com/stevdza/san/MyApp.kt
@@ -0,0 +1,27 @@
+package com.stevdza.san
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.core.App
+import com.varabyte.kobweb.silk.init.InitSilk
+import com.varabyte.kobweb.silk.init.InitSilkContext
+import com.varabyte.kobweb.silk.SilkApp
+import com.varabyte.kobweb.silk.components.layout.AnimatedColorSurfaceVariant
+import com.varabyte.kobweb.silk.components.layout.Surface
+import org.jetbrains.compose.web.css.*
+
+@InitSilk
+fun updateTheme(ctx: InitSilkContext) {
+    // Configure silk here
+}
+
+@App
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    SilkApp {
+        Surface(Modifier.minWidth(100.vw).minHeight(100.vh), variant = AnimatedColorSurfaceVariant) {
+            content()
+        }
+    }
+}
