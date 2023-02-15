614e8293d357404118d65e4c1dc036347e1cf453
Stevdza-San <stefan.jovanavich@gmail.com>
2023-02-15 08:43:49 +0100

    Export the website

--- site/src/jsMain/kotlin/com/stevdza/san/model/EditorTheme.kt
+++ site/src/jsMain/kotlin/com/stevdza/san/model/EditorTheme.kt
@@ -2,6 +2,6 @@ package com.stevdza.san.model
 
 enum class EditorTheme(val color: String) {
     RoyalBlue(color = "linear-gradient(-45deg, #554CF4 0%, #16169C 100%)"),
-    VolcanoMagenta(color = "linear-gradient(-45deg, #CD5888 0%, #913175 100%)"),
-    VioletBeauty(color = "linear-gradient(-45deg, #C5008E 0%, #6F1AB6 100%)")
+//    VolcanoMagenta(color = "linear-gradient(-45deg, #CD5888 0%, #913175 100%)"),
+//    VioletBeauty(color = "linear-gradient(-45deg, #C5008E 0%, #6F1AB6 100%)")
 }
